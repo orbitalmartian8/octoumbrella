6e9280a34df5db2c70ded97a2bea671c753703b8
CJ <83363242+LinuxGamer@users.noreply.github.com>
2023-03-15 13:24:27 +0000

    push

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
-print("Hello World!")
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
+# Create the main window
+root = tk.Tk()
+root.title("Artist's App")
+
+# Add widgets
+# ...
+
+# Implement features
+# ...
+
+# Start the app
+root.mainloop()
